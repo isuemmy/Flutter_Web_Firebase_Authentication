932f038db3afffb71434189d0eda69136832d29c
isuemmy <63224702+isuemmy@users.noreply.github.com>
2020-07-21 06:27:33 +0100

    Add files via upload

---
+++ lib/app/constants/strings.dart
@@ -0,0 +1,11 @@
+class Strings {
+  // App
+  static const String appName = 'Firebase Auth Demo';
+
+  // Authentication
+  static const String signInMessage = 'Flutter Web Firebase Integeration';
+  static const String anonymousSignUp = 'Anonymous sign in';
+  static const String signOut = 'Sign out';
+
+  // General
+}
